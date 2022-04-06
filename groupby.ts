fcf849a1ef7fff693d2d43202df41101a39c4bb9
Daniel Frost <flint.bets@yandex.ru>
2022-04-06 13:50:35 +0300

    Fixed any & deleted object option

--- groupby.ts
+++ groupby.ts
@@ -1,27 +1,16 @@
-function groupByMap<ArrElem, CBResult>(arr: ArrElem[], callback: (elem: ArrElem) => CBResult): any {
-  let result: Map<CBResult, ArrElem[]> = new Map()
+function groupBy<ArrElem, CBResult>(arr: ArrElem[], callback: (elem: ArrElem) => CBResult): { [key: string]: ArrElem[] } {
+  let result: Map<CBResult, ArrElem[]> = new Map();
   arr.forEach((elem) => {
     const key: CBResult = callback(elem);
-    const resultItem: ArrElem[] | undefined = result.get(key)
-    resultItem ? result.set(key, [...resultItem, elem]) : result.set(key, [elem])
+    const resultItem: ArrElem[] | undefined = result.get(key);
+    resultItem ? result.set(key, [...resultItem, elem]) : result.set(key, [elem]);
   });
-  
+
   return Object.fromEntries(result.entries());
 }
 
-function groupByObj<ArrElem>(arr: ArrElem[], callback: (elem: ArrElem) => number): any {
-  let result: {[key: number]: ArrElem[]} = {}
-  arr.forEach((elem) => {
-    const key: number = callback(elem);
-    const resultItem: ArrElem[] | undefined = result[key]
-    resultItem ? result[key] = [...resultItem, elem] : result[key] = [elem]
-  });
-  
-  return result;
-}
-
-console.log(groupByObj([1.2, 1.1, 2.3, 0.4], Math.floor));
-console.log(groupByObj(['one', 'two', 'three'], (el) => el.length));
+console.log(groupBy([1.2, 1.1, 2.3, 0.4], Math.floor));
+console.log(groupBy(['one', 'two', 'three'], (el) => el.length));
 
 enum Gender {
   Male,
@@ -29,12 +18,12 @@ enum Gender {
 }
 
 console.log(
-  groupByObj(
+  groupBy(
     [
       { g: Gender.Male, n: 'A' },
       { g: Gender.Female, n: 'B' },
       { g: Gender.Female, n: 'C' },
     ],
-    (el) => el.g 
+    (el) => el.g
   )
 );
