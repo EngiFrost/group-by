f420b57b2dff094fa1026f170517f7e843129a86
Daniel Frost <flint.bets@yandex.ru>
2022-04-07 13:50:28 +0300

    Improved function performance

--- groupby.ts
+++ groupby.ts
@@ -1,4 +1,4 @@
-function groupBy<ArrElem, CBResult>(arr: ArrElem[], callback: (elem: ArrElem) => CBResult): { [key: string]: ArrElem[] } {
+const groupByMap = <ArrElem, CBResult>(arr: ArrElem[], callback: (elem: ArrElem) => CBResult): { [key: string]: ArrElem[] } => {
   let result: Map<CBResult, ArrElem[]> = new Map();
   arr.forEach((elem) => {
     const key: CBResult = callback(elem);
@@ -7,7 +7,18 @@ function groupBy<ArrElem, CBResult>(arr: ArrElem[], callback: (elem: ArrElem) =>
   });
 
   return Object.fromEntries(result.entries());
-}
+};
+
+const groupBy = <ArrElem, CBResult>(arr: ArrElem[], callback: (elem: ArrElem) => CBResult): { [key: string]: ArrElem[] } => {
+  let result: { [key: string]: ArrElem[] } = {};
+  arr.forEach((elem) => {
+    const key: string = String(callback(elem));
+    const resultItem: ArrElem[] | undefined = result[key];
+    resultItem ? (result[key] = [...resultItem, elem]) : (result[key] = [elem]);
+  });
+
+  return result;
+};
 
 console.log(groupBy([1.2, 1.1, 2.3, 0.4], Math.floor));
 console.log(groupBy(['one', 'two', 'three'], (el) => el.length));
